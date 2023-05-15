3f647581a3eaf2256558a7ad2123b8297aa65964
Flavio Santos <flavio.pasantos@outlook.com>
2023-05-15 14:28:30 -0300

    User entitie and resource

---
+++ src/main/java/flavio/spring/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package flavio.spring.course.resources;
+
+import flavio.spring.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
