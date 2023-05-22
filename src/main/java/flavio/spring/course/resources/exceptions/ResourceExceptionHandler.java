bcbe8b7faac80be29f1e5a20a9539c5f45a8968d
Flavio Santos <flavio.pasantos@outlook.com>
2023-05-22 14:15:12 -0300

    Exceptions impl

---
+++ src/main/java/flavio/spring/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package flavio.spring.course.resources.exceptions;
+
+import flavio.spring.course.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice //Intercepta excessões
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status, error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/flavio/spring/course/services/UserService.java
+++ src/main/java/flavio/spring/course/services/UserService.java
@@ -2,6 +2,7 @@ package flavio.spring.course.services;
 
 import flavio.spring.course.entities.User;
 import flavio.spring.course.repositories.UserRepository;
+import flavio.spring.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
